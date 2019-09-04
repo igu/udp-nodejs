92f75547466fa3259b7ff2d011dec47c733d230e
Igor Alisson <igoralissoncn@gmail.com>
2019-09-04 19:56:34 -0300

    Adicionando arquivos iniciais

---
+++ src/udp_client.js
@@ -0,0 +1,17 @@
+const PORT = 3333;
+const HOST = 'localhost';
+
+const dgram = require('dgram');
+const client = dgram.createSocket('udp4');
+
+const msg = 'ok';
+
+client.send(msg, 0, msg.length, PORT, HOST, function(err, bytes) {
+  if (err) throw err;
+  console.log(`Conexão iniciada com ${HOST}:${PORT}...`);
+});
+
+client.on('message', function(message, remote) {
+    console.log(`Mensagem recebida de ${remote.address}:${remote.port} - Msg: ${message} / Msg length: ${message.length}`);
+    client.close();
+});
---
+++ src/udp_server.js
@@ -0,0 +1,24 @@
+const PORT = 3333;
+const HOST = 'localhost';
+
+const dgram = require('dgram');
+const server = dgram.createSocket('udp4');
+
+server.on('listening', function() {
+  const address = server.address();
+  console.log(`UDP SERVER ON -> ${address.address}:${address.port}`);
+});
+
+server.on('message', function(message, remote) {
+    console.log(`Nova conexão: ${remote.address}:${remote.port} - Msg: ${message}`);
+    const final = 'Meu nome é Igorr';
+    let PORT_CLIENT = remote.port;
+    let HOST_CLIENT  = remote.address;
+    server.send(final, 0, final.length, PORT_CLIENT, HOST_CLIENT, function(err, bytes) {
+        if (err) throw err;
+        console.log(`Mensagem enviada para ${HOST_CLIENT}:${PORT_CLIENT}`);
+        console.log(`Tamanho: ${final.length}`);
+    });
+});
+
+server.bind(PORT, HOST);
